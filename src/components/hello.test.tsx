9714ad1c4b27d753b9cd0e4c07fc8b3c44ed513c
khizerdev <46632721+khizerdev@users.noreply.github.com>
2023-12-14 12:39:57 +0500

    Grouping Tests

--- src/components/hello.test.tsx
+++ src/components/hello.test.tsx
@@ -1,14 +1,18 @@
 import { render, screen } from "@testing-library/react";
 import Hello from "./hello";
 
-test("Hello renders correctly", () => {
-  render(<Hello />);
-  const textElement = screen.getByText(/hello/i);
-  expect(textElement).toBeInTheDocument();
-});
+describe("Hello", () => {
+  test("renders correctly", () => {
+    render(<Hello />);
+    const textElement = screen.getByText(/hello/i);
+    expect(textElement).toBeInTheDocument();
+  });
 
-test("Hello renders with a name", () => {
-  render(<Hello name="John" />);
-  const textElement = screen.getByText("Hello John");
-  expect(textElement).toBeInTheDocument();
+  describe("With name", () => {
+    test("renders correctly", () => {
+      render(<Hello name="John" />);
+      const textElement = screen.getByText("Hello John");
+      expect(textElement).toBeInTheDocument();
+    });
+  });
 });
