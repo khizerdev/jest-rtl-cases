63e300b7618c0864bc781b949a6678ebabd802cc
khizerdev <46632721+khizerdev@users.noreply.github.com>
2023-12-20 22:14:48 +0500

    getByRole

---
+++ src/components/application/Application.test.tsx
@@ -0,0 +1,21 @@
+import { render, screen } from "@testing-library/react";
+import { Application } from "./Application";
+
+describe("Render correctly", () => {
+  test("render application component correctly", () => {
+    render(<Application />);
+    const nameElement = screen.getByRole("textbox", {
+      name: "Name",
+    });
+    expect(nameElement).toBeInTheDocument();
+
+    const jobLocationElement = screen.getByRole("combobox");
+    expect(jobLocationElement).toBeInTheDocument();
+
+    const termsElement = screen.getByRole("checkbox");
+    expect(termsElement).toBeInTheDocument();
+
+    const submitElement = screen.getByRole("button");
+    expect(submitElement).toBeInTheDocument();
+  });
+});
