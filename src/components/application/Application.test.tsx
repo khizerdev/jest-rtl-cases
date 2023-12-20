52d23f5ea476f2b59af47a23272dcef422380fd5
khizerdev <46632721+khizerdev@users.noreply.github.com>
2023-12-21 00:40:15 +0500

    getByAltText, getByTitle, getByTestId added

--- src/components/application/Application.test.tsx
+++ src/components/application/Application.test.tsx
@@ -56,5 +56,20 @@ describe('Render correctly', () => {
 
     const nameElement4 = screen.getByDisplayValue('Syed');
     expect(nameElement4).toBeInTheDocument();
+
+    /** getByAltText */
+
+    const imageElement = screen.getByAltText('a person with a laptop');
+    expect(imageElement).toBeInTheDocument();
+
+    /** getByTitle */
+
+    const closeElement = screen.getByTitle('close');
+    expect(closeElement).toBeInTheDocument();
+
+    /** getByTestId */
+
+    const customElement = screen.getByTestId('custom-element');
+    expect(customElement).toBeInTheDocument();
   });
 });
