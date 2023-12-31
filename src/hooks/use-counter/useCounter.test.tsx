f421c231271e4484c0cdd288755ca2405a08ab40
khizerdev <46632721+khizerdev@users.noreply.github.com>
2023-12-31 23:26:32 +0500

    Act Utility added

--- src/hooks/use-counter/useCounter.test.tsx
+++ src/hooks/use-counter/useCounter.test.tsx
@@ -13,4 +13,16 @@ describe('useCounter', () => {
     });
     expect(result.current.count).toBe(10);
   });
+
+  test('should increment the count', () => {
+    const { result } = renderHook(useCounter);
+    act(() => result.current.increment());
+    expect(result.current.count).toBe(1);
+  });
+
+  test('should decrement the count', () => {
+    const { result } = renderHook(useCounter);
+    act(() => result.current.decrement());
+    expect(result.current.count).toBe(-1);
+  });
 });
