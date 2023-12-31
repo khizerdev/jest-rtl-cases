4dd022c893ba62e6e3674d42d48945788d0c6e24
khizerdev <46632721+khizerdev@users.noreply.github.com>
2023-12-31 21:03:30 +0500

    Custom Render Functions

--- src/components/mui/MuiMode.test.tsx
+++ src/components/mui/MuiMode.test.tsx
@@ -1,20 +1,15 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen } from '../../test-utils';
 import { MuiMode } from './MuiMode';
-import { AppProviders } from '../../providers/AppProviders';
 
 describe('MuiMode', () => {
   test('renders text correctly', () => {
-    render(<MuiMode />, {
-      wrapper: AppProviders,
-    });
+    render(<MuiMode />);
     const headingElement = screen.getByRole('heading');
     expect(headingElement).toHaveTextContent('dark mode');
   });
 
   test('renders text in white color for dark mode', () => {
-    render(<MuiMode />, {
-      wrapper: AppProviders,
-    });
+    render(<MuiMode />);
     const headingElement = screen.getByRole('heading');
     expect(headingElement).toHaveStyle({ color: "'rgb(255, 255, 255)'" });
   });
