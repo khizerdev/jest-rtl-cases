2a574ec616efcfbe2e79de5116536c1b966e8e4f
khizerdev <46632721+khizerdev@users.noreply.github.com>
2024-01-03 00:36:18 +0500

    Mock HTTP request

--- src/components/users/Users.test.tsx
+++ src/components/users/Users.test.tsx
@@ -7,4 +7,9 @@ describe('Users', () => {
     const textElement = screen.getByText('Users');
     expect(textElement).toBeInTheDocument();
   });
+  test('renders a list of users', async () => {
+    render(<Users />);
+    const users = await screen.findAllByRole('listitem');
+    expect(users).toHaveLength(3);
+  });
 });
